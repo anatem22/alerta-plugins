e971320dfd45319b2b9ff70fcb101a3c5f057df6
anatem22 <81459830+anatem22@users.noreply.github.com>
2021-05-14 00:17:43 +0700

    Update routing.py

--- plugins/routing/routing.py
+++ plugins/routing/routing.py
@@ -1,5 +1,36 @@
+#def rules(alert, plugins):
+#    if alert.severity in ['critical', 'major']:
+#        return [plugins['telegram']]
+#    else:
+#        return [plugins['rocketchat']]
+"""
+Example Routing Rules Plugin - Customer-based Escalation/Notification
+Use different notification paths for different customers. ie. PagerDuty
+for Tier 1 customers, Slack for Tier 2 and nothing for Tier 3 customers.
+"""
+
+TIER_ONE_CUSTOMERS = [
+    'critical',
+    'major'
+]
+
+TIER_TWO_CUSTOMERS = [
+    'minor',
+    'warning'
+]
+
+
 def rules(alert, plugins):
-    if alert.severity in ['critical', 'major']:
-        return [plugins['telegram']]
+
+    if alert.customer in TIER_ONE_CUSTOMERS:
+        return [
+            plugins['telegram']
+        ]
+    elif alert.customer in TIER_TWO_CUSTOMERS:
+        return [
+            plugins['rocketchat']
+        ]
     else:
-        return [plugins['rocketchat']]
+        return [
+            plugins['reject']
+        ]
